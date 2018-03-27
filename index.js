23f64aae5c0c7416a306384a3a0dfedef49a7c56
Emonadeo <emanuel-pilz@web.de>
2018-03-28 01:58:37 +0200

    Added GitHub flavored Markdown

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-
 module.exports = {
-    hooks: {
-        config: function(config) {
-            config.styles = config.styles || config.pluginsConfig['theme-default'].styles;
+  website: {
+    assets: './_assets/website',
+    js: [
+      'gitbook.js',
+      'theme.js'
+    ],
+    css: [
+      'style.css'
+    ]
+  },
+  hooks: {
+    config: function(config) {
+      config.styles = config.styles || config.pluginsConfig['theme-github'].styles;
 
-            return config;
-        }
+      return config;
     }
+  }
 };
-
-
