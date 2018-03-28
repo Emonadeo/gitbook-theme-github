b4c8838eaaeda62a13d6c32ba804765ab0f2d94e
Emonadeo <emanuel-pilz@web.de>
2018-03-28 04:17:31 +0200

    Added Style

--- src/build.sh
+++ src/build.sh
@@ -3,9 +3,5 @@
 # Cleanup folder
 rm -rf _assets
 
-# Recreate folder
-# mkdir -p _assets/website/
-# mkdir -p _assets/ebook/
-
 # Compile styles
 lessc -clean-css src/less/style.less _assets/style.css
